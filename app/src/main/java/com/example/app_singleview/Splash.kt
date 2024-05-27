c41c20907dbf57c4f11da31b2ddb40a0b655c717
ari-me <119844094+ari-me@users.noreply.github.com>
2024-05-27 23:34:51 +0400

    Update Splash.kt

--- app/src/main/java/com/example/app_singleview/Splash.kt
+++ app/src/main/java/com/example/app_singleview/Splash.kt
@@ -12,16 +12,16 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        //using window.setflags to hide the taskbar and display splash screen as full screen
+        //Hides the taskbar and displays Splash Screen as full screen
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        //Using Hander().postDelayed({}) & postDelayed(Runnable, tine) method for delay time message
+        //To make the Splash Screen a delayed short view
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 4000) // 4000 is hte delayed time in milliseconds
+        }, 4000)
     }
 }
